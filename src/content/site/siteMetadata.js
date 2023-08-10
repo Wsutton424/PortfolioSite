8974cc3a595384c4d1aa12baf42b266b2c580d49
Wsutton424 <wsutton@email.sc.edu>
2023-08-09 23:02:46 -0400

    updated Site Metadata

--- src/content/site/siteMetadata.js
+++ src/content/site/siteMetadata.js
@@ -1,11 +1,11 @@
 // The title in the web browser tab
-export const SITE_TITLE = "Template Website";
+export const SITE_TITLE = "Portfolio Website";
 
 // Site description
-export const SITE_DESC = "Portfolio Website Template";
+export const SITE_DESC = "Portfolio Website for William Sutton";
 
 // Comma separated list of descriptors for the site
-export const SITE_KEYWORDS = "template, portfolio, high school, college, project, admissions, resume";
+export const SITE_KEYWORDS = "job, portfolio, high school, college, project, admissions, resume";
 
 // Canonical site URL
-export const SITE_URL = "https://josh-beck.github.io/Portfolio-Website-Template/";
+export const SITE_URL = "https://github.com/Wsutton424/PortfolioSite";
