bbbe9debcec5a24d7b35f808a0450f0935da56c2
Wsutton424 <wsutton@email.sc.edu>
2023-08-17 00:41:10 -0400

    Updating web config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-    pathPrefix: `/`,
+    pathPrefix: "/PortfolioSite",
     plugins: [
         'gatsby-plugin-postcss',
         `gatsby-plugin-image`,
